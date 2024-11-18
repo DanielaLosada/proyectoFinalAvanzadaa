cd42569213bd7ad5aee9a26c055812d6024541e7
XimenaRuiz26 <angieruiz564@gmail.com>
2024-11-17 22:08:16 -0500

    Ajustos en Config, Dockerfile y properties para despliegue

--- src/main/java/co/edu/uniquindio/UniEventos/config/FirebaseConfig.java
+++ src/main/java/co/edu/uniquindio/UniEventos/config/FirebaseConfig.java
@@ -4,16 +4,17 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
     public FirebaseApp intializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(
-                "src/main/resources/unieventos-54199-firebase-adminsdk-6wry8-660f3c2e02.json"
-        );
+        InputStream serviceAccount = new ClassPathResource("unieventos-54199-firebase-adminsdk-6wry8-660f3c2e02.json").getInputStream();
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket("unieventos-54199.appspot.com")
